c38bfaee26bc6266bb9ff971f00b4f070496d578
Dongqi <pudongqi@gmail.com>
2023-07-12 10:52:54 +0200

    Update lead_3.py

--- baselines/lead_3.py
+++ baselines/lead_3.py
@@ -1,19 +1,27 @@
-import nltk
 import json
+import os
+import random
+from nltk import tokenize
+import numpy as np
 from tqdm import tqdm
+
+# Set random seeds for reproducibility
+def set_seed(seed):
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
 
 def lead_three_baseline(input_data, n_sent=3):
     if not input_data:
         raise ValueError("Input data cannot be empty.")
     all_pairs = []
-    for pair in tqdm(input_data):
+    for pair in tqdm(input_data, desc='Processing input data'):
         try:
             article = pair['article'].strip()
-
         except KeyError:
-            raise ValueError("Each dictionary in input data should contain 'reference' and 'prediction' keys.")
+            raise ValueError("Each dictionary in input data should contain 'article' and 'reference' keys.")
         
-        article_sents = nltk.tokenize.sent_tokenize(article)
+        article_sents = tokenize.sent_tokenize(article)
         if len(article_sents) >= n_sent:
             pair['prediction'] = ' '.join(article_sents[:n_sent])
         else:
@@ -22,13 +30,20 @@ def lead_three_baseline(input_data, n_sent=3):
     return all_pairs
 
 if __name__ == "__main__":
+    # Set the random seeds
+    set_seed(2023)
 
-    with open("test.json") as f:
-        test_data=json.load(f)
+    # Set the input and output file paths
+    input_file = "test.json"
+    output_file = "lead_3_output.json"
+
+    with open(input_file) as f:
+        test_data = json.load(f)
 
     test_data = [{"article":instance['Paper_Body'], "reference": instance['News_Body']} for instance in test_data]
     test_output = lead_three_baseline(test_data)
 
-    with open("lead_3_output.json", 'w') as f:
+    with open(output_file, 'w') as f:
         json.dump(test_output, f)
+
 
