df89e7c37d35310d94685b5bb54c26cc97e8e6bf
Ewanwong <903327884@qq.com>
2023-07-16 23:55:45 +0200

    uploaded textrank and lexrank baselines

---
+++ baselines/lexrank_baseline.py
@@ -0,0 +1,57 @@
+# pip install lexrank
+from lexrank import LexRank
+
+from lexrank.mappings.stopwords import STOPWORDS
+import json
+import os
+import random
+from nltk import tokenize
+import numpy as np
+from tqdm import tqdm
+
+# Set random seeds for reproducibility
+def set_seed(seed):
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+
+def lexrank_baseline(input_data, lxr, n_sents=3):
+    if not input_data:
+        raise ValueError("Input data cannot be empty.")
+    all_pairs = []
+    for pair in tqdm(input_data, desc='Processing input data'):
+        try:
+            article = pair['article'].strip()
+        except KeyError:
+            raise ValueError("Each dictionary in input data should contain 'article' and 'reference' keys.")
+        
+        article_sents = tokenize.sent_tokenize(article)
+        summary = lxr.get_summary(article_sents, summary_size=n_sents, threshold=.1)
+        pair['prediction'] = ' '.join(summary)
+        all_pairs.append(pair)
+    return all_pairs
+
+if __name__ == "__main__":
+    # Set the random seeds
+    set_seed(2023)
+
+    # Set the input and output file paths
+    input_file = "test.json"
+    output_file = "lexrank_output.json"
+
+    # learn tf-idf on training data
+    train_file = "val.json"
+    with open(train_file) as f:
+        train_data = json.load(f)
+    documents = [instance['Paper_Body'] for instance in train_data][:3]
+
+    lxr = LexRank(documents, stopwords=STOPWORDS['en'])
+
+    with open(input_file) as f:
+        test_data = json.load(f)
+
+    test_data = [{"article":instance['Paper_Body'], "reference": instance['News_Body']} for instance in test_data][:3]
+    test_output = lexrank_baseline(test_data, lxr, n_sents=3) # set sentence number
+
+    with open(output_file, 'w') as f:
+        json.dump(test_output, f, indent=4)
---
+++ baselines/textrank.py
@@ -0,0 +1,46 @@
+# pip install summa
+
+from summa import summarizer
+import json
+import os
+import random
+from nltk import tokenize
+import numpy as np
+from tqdm import tqdm
+
+# Set random seeds for reproducibility
+def set_seed(seed):
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+
+def textrank_baseline(input_data, num_words=200):
+    if not input_data:
+        raise ValueError("Input data cannot be empty.")
+    all_pairs = []
+    for pair in tqdm(input_data, desc='Processing input data'):
+        try:
+            article = pair['article'].strip()
+        except KeyError:
+            raise ValueError("Each dictionary in input data should contain 'article' and 'reference' keys.")
+        
+        pair['prediction'] = summarizer.summarize(article, words=num_words).strip()
+        all_pairs.append(pair)
+    return all_pairs
+
+if __name__ == "__main__":
+    # Set the random seeds
+    set_seed(2023)
+
+    # Set the input and output file paths
+    input_file = "test.json"
+    output_file = "textrank_output.json"
+
+    with open(input_file) as f:
+        test_data = json.load(f)
+
+    test_data = [{"article":instance['Paper_Body'], "reference": instance['News_Body']} for instance in test_data]
+    test_output = textrank_baseline(test_data, num_words=200)
+
+    with open(output_file, 'w') as f:
+        json.dump(test_output, f, indent=4)
